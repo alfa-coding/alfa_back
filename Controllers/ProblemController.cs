626a1f54b75ce52c091c10bc3b15c97633b000cf
DimasVeliz <trevor90094@gmail.com>
2022-03-04 14:58:45 +0100

    Problem crud, done

--- Controllers/ProblemController.cs
+++ Controllers/ProblemController.cs
@@ -28,5 +28,32 @@ namespace alfa_back.Controllers
         {
             return ProblemContext.GetElements();
         }
+        // GET api/<CatController>/5
+        [HttpGet("{id}")]
+        public Problem Get(string id)
+        {
+            return this.ProblemContext.GetElementById(id);
+        }
+
+        // POST api/<CatController>
+        [HttpPost]
+        public void Post([FromBody] Problem value)
+        {
+            this.ProblemContext.InsertElement(value);
+        }
+
+        // PUT api/<CatController>/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Problem value)
+        {
+            this.ProblemContext.Update(id, value);
+        }
+
+        // DELETE api/<CatController>/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            this.ProblemContext.RemoveElement(id);
+        }
     }
 }
