21dc80fe73690807ca80230c2b9efdec21d4038b
DimasVeliz <trevor90094@gmail.com>
2022-03-05 19:34:54 +0100

    formaters crud added

--- Controllers/ProblemController.cs
+++ Controllers/ProblemController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using alfa_back.Infrastructure;
-using alfa_back.Models;
+using alfa_back.lib.Infrastructure;
+using alfa_back.lib.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
